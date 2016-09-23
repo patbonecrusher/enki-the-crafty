d849caa57c2e5dab9f12ce1c298f1521021104bb
Patrick Laplante <laplante.patrick@gmail.com>
2016-09-23 16:46:49 -0400

    added some more sourcing stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env zsh
 
+# -----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <bonecrusher@covenofchaos.com>  wrote this file.  As long as  you retain this
+# notice you can do whatever you want with this stuff.  If we meet some day and
+# you think this stuff is worth it, you can buy me a beer in return.
+#  > BoneCrusher <
+# -----------------------------------------------------------------------------
+[[ -z $1 ]] && { echo "You must specify a config file"; exit -1; }
+source $1
+
 mydir=${0:a:h}
+
+function setup_omyzsh {
+  if [[ ! -d "${mydir}/.oh-my-zsh" ]]; then
+  	(cd "${mydir}" && git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh)
+  else
+  	(cd "${mydir}/.oh-my-zsh" && git pull origin master)
+  fi
+}
 
 function setup_dotfile {
 
