bdb64eca3125d1bf589623fff9bbc02391dd3abe
Patrick Laplante <laplante.patrick@gmail.com>
2019-12-25 15:57:02 -0500

    wip

--- scripts/source/zsh.zsh
+++ scripts/source/zsh.zsh
@@ -18,6 +18,7 @@ zstyle ':completion:*' group-name''
 #     zle reset-prompt
 # }
 
+alias change="code ~/.zshrc"
 alias reload='source ~/.zshrc'
 autoload -U compinit
 compinit
@@ -129,3 +130,35 @@ alias -g XG='| xargs egrep'
 alias -g X='| xargs'
 
 alias normalize_path='typeset -aU path'
+
+
+# Automatically switch node versions when a directory has a `.nvmrc` file
+autoload -U add-zsh-hook
+# Zsh hook function
+load-nvmrc() {
+    # echo boo
+    # local node_version="$(nvm version)" # Current node version
+    # local nvmrc_path="$(nvm_find_nvmrc)" # Path to the .nvmrc file
+
+    # # Check if there exists a .nvmrc file
+    # if [ -n "$nvmrc_path" ]; then
+    # local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    # # Check if the node version in .nvmrc is installed on the computer
+    # if [ "$nvmrc_node_version" = "N/A" ]; then
+    #     # Install the node version in .nvmrc on the computer and switch to that node version
+    #     nvm install
+    # # Check if the current node version matches the version in .nvmrc
+    # elif [ "$nvmrc_node_version" != "$node_version" ]; then
+    #     # Switch node versions
+    #     nvm use
+    # fi
+    # # If there isn't an .nvmrc make sure to set the current node version to the default node version
+    # elif [ "$node_version" != "$(nvm version default)" ]; then
+    # echo "Reverting to nvm default version"
+    # nvm use default
+    # fi
+}
+# Add the above function when the present working directory (pwd) changes
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
