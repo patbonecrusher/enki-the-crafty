af7b1ecf75d33033b2d8ca17c035dc5fd1f673a5
Pat Laplante <plaplante@d3engineering.com>
2020-01-07 14:32:58 -0500

    backup

--- scripts/source/tsc.zsh
+++ scripts/source/tsc.zsh
@@ -11,4 +11,6 @@ mydir=`dirname $mydir`
 
 
 alias tsc110='export TSC_PATH=/home/pat/Projects/tsc/1.1.0 && export MANIF=102_master_1_1.xml && desk . tsc'
+alias tscmm2a='export TSC_PATH=/home/pat/Projects/tsc/mm_2 && export MANIF=102_master_mm_2.xml && desk . tsc'
 alias tscmm2='export TSC_PATH=/home/pat/Projects/tsc/mm_2 && export MANIF=102_master_mm_2.xml && desk . tsc'
+alias tsc120='export TSC_PATH=/home/pat/Projects/tsc/1_2 && export MANIF=102_release_1_2.xml && desk . tsc'
