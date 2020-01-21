dae48ab643bfaf9b5a7cdfef348e3c9da9595b08
Pat Laplante <laplante.patrick@gmail.com>
2020-01-21 16:25:45 -0500

    wip

--- scripts/source/tsc.zsh
+++ scripts/source/tsc.zsh
@@ -14,4 +14,4 @@ alias tsc110='export TSC_PATH=/home/pat/Projects/tsc/1.1.0 && export MANIF=102_m
 alias tscmm2='export TSC_PATH=/home/pat/Projects/tsc/mm_2 && export MANIF=102_master_mm_2.xml && desk . tsc'
 
 alias repo-init='repo init -u ssh://plaplante@gerrit.d3engineering.com:29418/tsc/caf/platform/manifest -b d3/dev/open_q_820 -m'
-alias sync='repo sync -c --no-tags -j32'
+alias repo-sync='repo sync -c --no-tags -j32'
